b5dd6b2ea36b99914f642bf716710757e0c2599f
Glitch (file-metadata-microservice-fcc-rosa) <none>
2019-08-20 01:22:24 +0000

    🚂👾 Updated with Glitch

--- server.js
+++ server.js
@@ -4,18 +4,22 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
+var multer = require('multer');
+var upload = multer();
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) =>{
      res.sendFile(process.cwd() + '/views/index.html');
   });
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+// where the upload btn is gonna be redirected
+app.post('/api/fileanalyse',upload.fields(['upfile']),(req,res)=>{
+  
+  res.send('something is happening!!');
 });
 
 app.listen(process.env.PORT || 3000, function () {
