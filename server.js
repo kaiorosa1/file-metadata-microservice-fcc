5caf0c8e6813b6c6e13d193dee3d5143da362f42
Glitch (file-metadata-microservice-fcc-rosa) <none>
2019-10-21 02:28:04 +0000

    🚀👩‍❤️‍💋‍👩 Updated with Glitch

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var cors = require('cors');
+var express = require("express");
+var cors = require("cors");
 
 // require and use "multer"...
-var multer = require('multer');
+var multer = require("multer");
 var upload = multer();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', (req, res) =>{
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-// where the upload btn is gonna be redirected
-app.post('/api/fileanalyse',upload.fields(['upfile']),(req,res)=>{
-  
-  res.send('something is happening!!');
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+
+// where the upload btn is gonna be redirected
+app.post("/api/fileanalyse", upload.fields([{"name": "upfile"}]), (req, res) => {
+  // show what is inside the upfile file
+  res.send("something is happening!!");
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Node.js listening ...");
 });
